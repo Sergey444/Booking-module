0c2f4ef4cfa9d44c4aaf494a82ed411f070f0220
Sergey444 <autolife724@gmail.com>
2019-07-29 15:04:29 +0500

    ajusted filter

--- dist/main.js
+++ dist/main.js
@@ -908,15 +908,15 @@ __webpack_require__.r(__webpack_exports__);
     BX24.callBatch({
       first: ['crm.deal.list', {
         'filter': {
-          ">=UF_CRM_1563776665746": date.start,
-          '<=UF_CRM_1563776665746': date.end,
+          "<=UF_CRM_1563776654352": date.start,
+          '>=UF_CRM_1563776665746': date.start,
           '!UF_CRM_1563881923': false
         },
         'select': ["UF_CRM_1563881923"]
       }],
       second: ['crm.deal.list', {
         'filter': {
-          "<=UF_CRM_1563776654352": date.start,
+          "<=UF_CRM_1563776654352": date.end,
           '>=UF_CRM_1563776665746': date.end,
           '!UF_CRM_1563881923': false
         },
--- src/js/find-free-company.js
+++ src/js/find-free-company.js
@@ -7,12 +7,13 @@
 export default (date) => {
     return new Promise((resolve) => {
         BX24.callBatch({
-            first : ['crm.deal.list', { 'filter': { ">=UF_CRM_1563776665746": date.start, '<=UF_CRM_1563776665746': date.end, '!UF_CRM_1563881923': false }, 'select': ["UF_CRM_1563881923"] }],
-            second: ['crm.deal.list', { 'filter': { "<=UF_CRM_1563776654352": date.start, '>=UF_CRM_1563776665746': date.end, '!UF_CRM_1563881923': false }, 'select': ["UF_CRM_1563881923"] }],
+            first : ['crm.deal.list', { 'filter': { "<=UF_CRM_1563776654352": date.start, '>=UF_CRM_1563776665746': date.start, '!UF_CRM_1563881923': false }, 'select': ["UF_CRM_1563881923"] }],
+            second: ['crm.deal.list', { 'filter': { "<=UF_CRM_1563776654352": date.end, '>=UF_CRM_1563776665746': date.end, '!UF_CRM_1563881923': false }, 'select': ["UF_CRM_1563881923"] }],
         }, 
         (result) => {
             const deals = result.first.data().concat(result.second.data());
             return resolve(deals);
         });
+
     });
 }
